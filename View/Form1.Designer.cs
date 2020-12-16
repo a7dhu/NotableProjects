5ca5c7d2b86ac316430967b14986bbc415b1531b
a7dhu <51385300+a7dhu@users.noreply.github.com>
2020-12-16 13:03:35 -0700

    Add files via upload

---
+++ View/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerName = new System.Windows.Forms.TextBox();
+            this.playerNameLabel = new System.Windows.Forms.Label();
+            this.hostnameLabel = new System.Windows.Forms.Label();
+            this.hostname = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // playerName
+            // 
+            this.playerName.Location = new System.Drawing.Point(120, 10);
+            this.playerName.Name = "playerName";
+            this.playerName.Size = new System.Drawing.Size(100, 22);
+            this.playerName.TabIndex = 0;
+            // 
+            // playerNameLabel
+            // 
+            this.playerNameLabel.AutoSize = true;
+            this.playerNameLabel.Location = new System.Drawing.Point(13, 13);
+            this.playerNameLabel.Name = "playerNameLabel";
+            this.playerNameLabel.Size = new System.Drawing.Size(101, 17);
+            this.playerNameLabel.TabIndex = 1;
+            this.playerNameLabel.Text = "Player Name : ";
+            // 
+            // hostnameLabel
+            // 
+            this.hostnameLabel.AutoSize = true;
+            this.hostnameLabel.Location = new System.Drawing.Point(280, 13);
+            this.hostnameLabel.Name = "hostnameLabel";
+            this.hostnameLabel.Size = new System.Drawing.Size(49, 17);
+            this.hostnameLabel.TabIndex = 2;
+            this.hostnameLabel.Text = "Host : ";
+            // 
+            // hostname
+            // 
+            this.hostname.Location = new System.Drawing.Point(335, 10);
+            this.hostname.Name = "hostname";
+            this.hostname.Size = new System.Drawing.Size(100, 22);
+            this.hostname.TabIndex = 3;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(482, 10);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 4;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.hostname);
+            this.Controls.Add(this.hostnameLabel);
+            this.Controls.Add(this.playerNameLabel);
+            this.Controls.Add(this.playerName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox playerName;
+        private System.Windows.Forms.Label playerNameLabel;
+        private System.Windows.Forms.Label hostnameLabel;
+        private System.Windows.Forms.TextBox hostname;
+        private System.Windows.Forms.Button connectButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
